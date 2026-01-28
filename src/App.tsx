8bec4e0ff3fc0a6d1b195fd21653bdd30796975b
kesparleo <kespar299@gmail.com>
2026-01-28 23:52:57 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+
+  return (
+    <>
+      fontes:
+      <ol>
+        <li>
+          Comunities site: <a href="https://mozcomunidades.web.app/" target="_blank">Site</a>
+        </li>
+        <li>
+          Github: <a href="https://github.com/RichaldoElias/ComunidadesMoz?tab=readme-ov-file#QA-Community-Moz" target="_blank">Informations</a>
+        </li>
+      </ol>
+    </>
+  )
+}
+
+export default App
